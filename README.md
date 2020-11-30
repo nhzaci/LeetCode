a77ddd81145e75fc7fcecaa436f5bce711ac347e
nhzaci <warrior.killz@gmail.com>
2020-11-30 20:37:39 +0800

    Intialised repository

---
+++ README.md
@@ -0,0 +1,9 @@
+# Leetcode
+
+Welcome to the repository of my Leetcode solutions
+
+## Completed Problems
+
+| Problem         | Difficulty | Language |
+| --------------- | ---------- | -------- |
+| Add Two Numbers | Medium     | Python   |
---
+++ addTwoNumbers.py
@@ -0,0 +1,76 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    '''
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        final_node = ListNode(l1.val + l2.val)
+        first_node_pointer = final_node
+        carry = 0
+        while not l1.next is None and not l2.next is None:
+            next_l1_val = l1.next.val if not l1.next is None else 0
+            next_l2_val = l2.next.val if not l2.next is None else 0
+            final_node.next = ListNode(
+                (next_l1_val + next_l2_val + carry) % 10)
+            carry = (next_l1_val + next_l2_val + carry) // 10
+            l1 = l1.next
+            l2 = l2.next
+            final_node = final_node.next
+        return first_node_pointer
+    '''
+
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # create the first list nodes with values
+        first_sum = (l1.val + l2.val) % 10
+        # initialise the first carry value
+        carry = (l1.val + l2.val) // 10
+        final_node = ListNode(first_sum)
+        first_node_pointer = final_node
+        i = 0
+        # for subsequent ones, while not last, continue initialising the values
+        while not l1.next is None or not l2.next is None:
+            next_l1_val = l1.next.val if not l1.next is None else 0
+            next_l2_val = l2.next.val if not l2.next is None else 0
+            final_node.next = ListNode(
+                (next_l1_val + next_l2_val + carry) % 10)
+            carry = (next_l1_val + next_l2_val + carry) // 10
+            l1 = l1.next if not l1.next is None else ListNode(0)
+            l2 = l2.next if not l2.next is None else ListNode(0)
+            final_node = final_node.next
+            i += 1
+        if carry != 0:
+            final_node.next = ListNode(carry)
+        return first_node_pointer
+
+
+if __name__ == '__main__':
+    l1 = ListNode(2)
+    l2 = ListNode(5)
+    l1.next = ListNode(4)
+    l2.next = ListNode(6)
+    l1.next.next = ListNode(3)
+    l2.next.next = ListNode(4)
+    solution = Solution()
+    answer = solution.addTwoNumbers(l1, l2)
+    while not answer is None:
+        print(answer.val)
+        answer = answer.next
+    l3 = ListNode(9)
+    l3.next = ListNode(9)
+    l3.next.next = ListNode(9)
+    l3.next.next.next = ListNode(9)
+    l3.next.next.next.next = ListNode(9)
+    l3.next.next.next.next.next = ListNode(9)
+    l3.next.next.next.next.next.next = ListNode(9)
+    l4 = ListNode(9)
+    l4.next = ListNode(9)
+    l4.next.next = ListNode(9)
+    l4.next.next.next = ListNode(9)
+    answer = solution.addTwoNumbers(l3, l4)
+    while not answer is None:
+        print(answer.val)
+        answer = answer.next
