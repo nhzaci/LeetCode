6d8e4d523c24b137455a44fbba963461d3968357
nhzaci <warrior.killz@gmail.com>
2020-12-03 00:42:28 +0800

    completed letter combinations

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ Welcome to the repository of my Leetcode solutions
 | ----------------------------------------------------------------------------------------------------------------------------------- | ---------- | -------- | ----------- | ------------ |
 | [Add Two Numbers](https://github.com/nhzaci/LeetCode/blob/master/addTwoNumbers.py)                                                  | Medium     | Python   | O(n)        | O(n)         |
 | [Longest Substring WIthout Repeating](https://github.com/nhzaci/LeetCode/blob/master/longestSubStringWithoutRepeatingCharacters.py) | Medium     | Python   | O(n)        | O(n^2)       |
-| [Letter Combinations of Phone Number](https://github.com/nhzaci/LeetCode/blob/master/letterCombinationsPhoneNumber.py)              | Medium     | Python   | -           | -            |
+| [Letter Combinations of Phone Number](https://github.com/nhzaci/LeetCode/blob/master/letterCombinationsOfPhoneNumber.py)            | Medium     | Python   | O(n^2)      | O(n^2)       |
 | [Two Sum](https://github.com/nhzaci/LeetCode/blob/master/twoSum.py)                                                                 | Easy       | Python   | O(n)        | O(n)         |
--- letterCombinationsOfPhoneNumber.py
+++ letterCombinationsOfPhoneNumber.py
@@ -1,7 +1,34 @@
 class Solution:
     def letterCombinations(self, digits: str):
+        '''
+        Brute force solution -> iterate through all elements
+        Worst case time complexity: O(n^2)
+        Best case time complexity: O(n^2)
+        '''
         hashmap = {2: ['a', 'b', 'c'], 3: ['d', 'e', 'f'], 4: ['g', 'h', 'i'], 5: ['j', 'k', 'l'], 6: [
             'm', 'n', 'o'], 7: ['p', 'q', 'r', 's'], 8: ['t', 'u',  'v'], 9: ['w', 'x', 'y', 'z']}
         result = []
 
+        for digit in digits:
+            new_result = []
+
+            # for character in array stored in hashmap
+            for char in hashmap[int(digit)]:
+                # if result initialised, iterate through result array
+                if (len(result) > 0):
+                    for other in result:
+                        new_result.append(other + char)
+                # else, initialise result array
+                else:
+                    new_result.append(char)
+            # cache new result
+            result = new_result
+
         return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.letterCombinations("23"))
+    print(solution.letterCombinations("2"))
+    print(solution.letterCombinations(""))
