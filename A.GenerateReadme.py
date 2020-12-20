b1ebe23190a97acb436893d6aa397b5d39149eeb
nhzaci <warrior.killz@gmail.com>
2020-12-20 15:09:57 +0800

    chanage in file path

--- A.GenerateReadme.py
+++ A.GenerateReadme.py
@@ -3,7 +3,7 @@ import os
 file_list = sorted(list(
     filter(lambda name: '.py' in name and '-' in name, os.listdir())))
 
-with open('README.md', 'w') as f:
+with open('../../README.md', 'w') as f:
     f.write('# LeetCode Solutions\n\n')
     f.write('Welcome to the repository of my LeetCode solutions\n\n')
     f.write('## Completed Problems\n\n')
